d0e153eb86c146f21d40395d9539f07402cf5cb8
weimuhua <weimuhua0812@126.com>
2017-05-15 22:45:43 +0800

    views.py response with html page.

--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def func(request):
     return HttpResponse("This is my first django project!")
+
 
 def add(request):
     a = request.GET['a']
     b = request.GET['b']
     c = int(a) + int(b)
     return HttpResponse(str(c))
+
+
+def index(request):
+    return render(request, 'home.html')
