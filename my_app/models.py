1d1553f63353a78f78365f9a44665abd31c9526d
weimuhua <weimuhua0812@126.com>
2017-05-22 22:03:30 +0800

    在models.py中创建一个Person类。

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=25)
+    age = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
