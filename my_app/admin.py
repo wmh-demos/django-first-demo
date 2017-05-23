f9b1ac6bbc857c0f8741a6004ebb6d363fa4008d
weimuhua <weimuhua0812@126.com>
2017-05-24 00:31:06 +0800

    Django后台。

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from my_app.models import Article
+
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'update_time',)
+
+
+admin.site.register(Article, ArticleAdmin)
